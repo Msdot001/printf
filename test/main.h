588e67dab35d16a043f6362c02bb14d4719c9e40
Msdot001 <masudowolabi@gmail.com>
2023-10-17 14:06:59 -0700

    update main.c main.h

--- test/main.h
+++ test/main.h
@@ -6,5 +6,19 @@
 #include <stdlib.h>
 
 int _putchar(char c);
+int print_int(va_list arg);
+int print_unsigned(va_list arg);
 int _printf(const char *format, ...);
+int print_char(va_list arg);
+int print_str(va_list arg);
+int print_percent(void);
+
+
+
+
+
+
+
+
+
 #endif
