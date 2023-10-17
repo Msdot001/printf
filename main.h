2f2dfa06680db020cebe2599cf154138c0085b00
Msdot001 <masudowolabi@gmail.com>
2023-10-17 15:21:17 -0700

    printf

--- main.h
+++ main.h
@@ -1,13 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-
-#include <stdio.h>
+#include  <stdio.h>
+#include <limits.h>
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <limits.h>
 
 int _putchar(char c);
 int print_int(va_list arg);
@@ -37,3 +34,4 @@ int (*printer)(va_list);
 } identifierStruct;
 
 #endif
+
